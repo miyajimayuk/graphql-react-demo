3c1c34e18db3d59bb0ea730cfaff0146edab9eb0
anco5 <anco.dev5@gmail.com>
2019-07-10 23:56:36 +0900

    separate graphql code

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
 import React from 'react';
 import client from './client';
 import { ApolloProvider } from "react-apollo";
-import gql from 'graphql-tag';
 import { Query } from 'react-apollo';
-
-const ME = gql`
-    query me {
-        user(login: "miyajimayuk") {
-            login
-            avatarUrl
-       }
-    }
-`;
+import { ME } from './graphql';
 
 class App extends React.Component {
     render() {
         return (
             <ApolloProvider client={client}>
                 <div>GraphQL with React</div>
-                
+
                 <Query query={ME}>
                     {
                         ({ loading, error, data}) => {
@@ -26,7 +17,7 @@ class App extends React.Component {
                             if (error) return `Error! ${error.message}`;
                             console.log(data);
 
-                            return <div>ログイン名 : {data.user.login}</div>
+                            return <div>{data.user.login}</div>
                         }
                     }
                 </Query>
